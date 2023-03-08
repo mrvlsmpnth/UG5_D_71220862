b5a2df7f1e636de89ae2c5632f339a4e5fc0e9c2
mrvlsmpnth <118042978+mrvlsmpnth@users.noreply.github.com>
2023-03-08 18:46:55 +0700

    Add files via upload

---
+++ UG5_D_71220862/1_D_71220862.py
@@ -0,0 +1,13 @@
+def ganti_kata(b,c):
+        print(a)
+
+
+
+a= input("Masukkan kalimat : ")
+b= input("kata yang dicari: ")
+c= input("diganti menjadi: ")
+
+
+#fungsi :4
+for b in range(a):
+        print(c)
---
+++ UG5_D_71220862/3_D_71220862.py
@@ -0,0 +1,25 @@
+
+a =int(input("masukkan jarak awal (dalam meter): "))
+b =int(input("Masukkan sudut elevasi pada menit ke-5:"))
+c =int(input("Masukkan sudut elevasi pada menit ke-6:"))
+
+
+#Tinggi helikopter awalan :
+tinggi_awal= a*b
+
+#jarak helikopter akhir :
+tinggi_akhir= a*c-b
+
+#Selisih ketinggian helikopter
+selisih_tinggi= tinggi_akhir * b
+
+
+
+
+if a :("STOP","Stop","stop","berhenti","Berhenti","BERHENTI")
+print("Program dihentikan")
+
+for tinggi_awal in range(a,b,c):
+    print("ketinggian helikopter pada menit ke-5 adalah", tinggi_awal, "meter")
+    print("selisih ketinggian helikopter pada menit ke-5 dan ke-6 adalah", selisih_tinggi, "meter")
+
